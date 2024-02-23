a1188f19e9e6f81530e40968c820b2e1a977107d
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2024-02-23 17:52:18 +0100

    is done just drag and drop inPro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,81 +28,65 @@ export class AppComponent {
   ];
 
 
+   data: any[] = [
+    {
+      "Id": "Task 1",
+      "Title": "Task - 29001",
+      "Status": "InProgress",
+      "Summary": " issues reported by the customer.",
+      "Type": "Story",
+      "Priority": "High",
+      "Estimate": 1,
+      "Assignee": "Steven walker"
+  },
 
-
-  public data: Object[] = [  {
-    "Id": "Task 1",
-    "Title": "Task - 29001",
-    "Status": "Open",
-    "Summary": "Analyze the new requirements gathered from the customer.",
-    "Type": "Story",
-    "Priority": "Low",
-    "Estimate": 3.5,
-    "Assignee": "Nancy Davloio"
-},
-{
+  {
     "Id": "Task 2",
     "Title": "Task - 29002",
-    "Status": "In Progress",
-    "Summary": "Improve application performance",
-    "Type": "Improvement",
-    "Priority": "Normal",
-    "Estimate": 6,
-    "Assignee": "Andrew Fuller"
-},
-{
-  "Id": "Task 9",
-  "Title": "Task - 29009",
-  "Status": "In Progress",
-  "Summary": "Improve application ",
-  "Type": "Improvement",
-  "Priority": "Normal",
-  "Estimate": 6,
-  "Assignee": "Andrew Fuller"
-},
-{
-  "Id": "Task 11",
-  "Title": "Task - 29011",
-  "Status": "In Progress",
-  "Summary": "Improve performance",
-  "Type": "Improvement",
-  "Priority": "Normal",
-  "Estimate": 6,
-  "Assignee": "Andrew Fuller"
-},
-{
-    "Id": "Task 3",
-    "Title": "Task - 29003",
     "Status": "Open",
-    "Summary": "Arrange a web meeting with the customer to get new requirements.",
-    "Type": "Others",
-    "Priority": "Critical",
-    "Estimate": 5.5,
-    "Assignee": "Janet Leverling"
-},
-{
-    "Id": "Task 4",
-    "Title": "Task - 29004",
-    "Status": "In Progress",
-    "Summary": "Fix the issues reported in the IE browser.",
-    "Type": "Bug",
-    "Priority": "Critical",
-    "Estimate": 2.5,
-    "Assignee": "Janet Leverling"
-},
-{
-    "Id": "Task 5",
-    "Title": "Task - 29005",
-    "Status": "Review",
-    "Summary": "Fix the issues reported by the customer.",
-    "Type": "Bug",
-    "Priority": "Low",
-    "Estimate": "3.5",
+    "Summary": "  the customer.",
+    "Type": "Story",
+    "Priority": "High",
+    "Estimate": 1,
     "Assignee": "Steven walker"
 },
+
+{
+  "Id": "Task 3",
+  "Title": "Task - 29003",
+  "Status": "InProgress",
+  "Summary": "  the test and customer.",
+  "Type": "Story",
+  "Priority": "High",
+  "Estimate": 1,
+  "Assignee": "Steven walker"
+},
+
+{
+  "Id": "Task 4",
+  "Title": "Task - 29004",
+  "Status": "Close",
+  "Summary": "  test this part in task .",
+  "Type": "Story",
+  "Priority": "High",
+  "Estimate": 1,
+  "Assignee": "Steven walker"
+},
+
+{
+  "Id": "Task 5",
+  "Title": "Task - 29005",
+  "Status": "Open",
+  "Summary": "  test this part in task and i need complete  .",
+  "Type": "Story",
+  "Priority": "High",
+  "Estimate": 1,
+  "Assignee": "Steven walker"
+},
+
 {
     "Id": "Task 6",
-    "Title": "Task - 29007",
+    "Title": "Task - 29006",
     "Status": "Testing",
     "Summary": "Testing new requirements",
     "Type": "Improvement",
@@ -110,19 +94,21 @@ export class AppComponent {
     "Estimate": 1.5,
     "Assignee": "Robert King"
 },
+
 {
-    "Id": "Task 7",
-    "Title": "Task - 29009",
-    "Status": "Review",
-    "Summary": "Fix the issues reported in Safari browser.",
-    "Type": "Bug",
-    "Priority": "Critical",
-    "Estimate": 1.5,
-    "Assignee": "Nancy Davloio"
+  "Id": "Task 7",
+  "Title": "Task - 29007",
+  "Status": "Open",
+  "Summary": "  test this part in task and i need complete  .",
+  "Type": "Story",
+  "Priority": "High",
+  "Estimate": 1,
+  "Assignee": "Steven walker"
 },
+
 {
     "Id": "Task 8",
-    "Title": "Task - 29010",
+    "Title": "Task - 29008",
     "Status": "Close",
     "Summary": "Test the application in the IE browser.",
     "Type": "Story",
@@ -132,7 +118,7 @@ export class AppComponent {
 },
 {
     "Id": "Task 9",
-    "Title": "Task - 29011",
+    "Title": "Task - 29009",
     "Status": "Testing",
     "Summary": "Testing the issues reported by the customer.",
     "Type": "Story",
@@ -142,15 +128,32 @@ export class AppComponent {
 },
 {
     "Id": "Task 10",
-    "Title": "Task - 29015",
+    "Title": "Task - 29010",
     "Status": "Open",
     "Summary": "Show the retrieved data from the server in grid control.",
     "Type": "Story",
     "Priority": "High",
     "Estimate": 5.5,
     "Assignee": "Margaret hamilt"
-}
+},
+{
+  "Id": "Task 11",
+  "Title": "Task - 29011",
+  "Status": "Testing",
+  "Summary": "Show the retrieved data ",
+  "Type": "Story",
+  "Priority": "High",
+  "Estimate": 5.5,
+  "Assignee": "Margaret hamilt"
+},
+
 ];
+
+
+getTaskCount(keyField: string): number {
+  return this.data.filter(task => task.Status === keyField).length;
+}
+
 
 
 removeColumn(column: any): void {
